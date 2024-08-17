b67499fb689fc270ff77b72a30760b8b075d2d9e
julio_filizzola <juliofilizzola@hotmail.com>
2024-08-17 10:55:12 -0300

    Refactor: JSON response handling to log write errors
    Removed error return from WriteJsonResponse function and added logging for write errors. If a write error occurs, the function now also sends an internal server error response.

--- internal/utils/response.go
+++ internal/utils/response.go
@@ -1,16 +1,16 @@
 package utils
 
 import (
+	"log/slog"
 	"net/http"
 )
 
-func WriteJsonResponse(w http.ResponseWriter, statusCode int, response []byte) error {
+func WriteJsonResponse(w http.ResponseWriter, statusCode int, response []byte) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	_, err := w.Write(response)
 
-	if err != nil {
-		return err
+	if _, err := w.Write(response); err != nil {
+		slog.Error("error writing response", err)
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 	}
-	return nil
 }
